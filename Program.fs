eda7d726696206240c9d8b29876eda277ab9c920
Ben Hoffman <Ben Hoffman>
2016-11-07 20:11:29 -0700

    final minisuave project

--- Program.fs
+++ Program.fs
@@ -1,7 +1,24 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module MiniSuave.MiniSuave
+open Suave.Http
+open Suave.Console
+open Suave.Successful
+open Suave.Combinators
+open Suave.Filters
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let request = {Route = ""; Type = Suave.Http.GET}
+    let response = {Content = ""; StatusCode = 200}
+    let context = {Request = request; Response = response}
+
+    let app = Choose [
+                GET >=> Path "/hello" >=> OK "Hello GET"
+                POST >=> Path "/hello" >=> OK "Hello POST"
+                Path "/foo" >=> Choose [
+                                  GET >=> OK "Foo GET"
+                                  POST >=> OK "Foo POST" 
+                                ]
+              ]
+    executeInLoop context app
+
+    0
---
+++ Suave.fs
@@ -0,0 +1,101 @@
+﻿namespace Suave
+
+module Http = 
+    type RequestType = GET | POST
+    type Request = {
+        Route: string
+        Type: RequestType
+    }
+    type Response = {
+        Content: string
+        StatusCode: int
+    }
+    type Context = {
+        Request: Request
+        Response: Response
+    }
+    type WebPart = Context -> Async<Context option>
+
+module Successful =
+    open Http
+
+    let OK content context =
+        {context with Response = {Content = content; StatusCode = 200}}
+        |> Some
+        |> async.Return
+
+module Console =
+    open Http
+    let execute inputContext webpart =
+        async {
+            let! outputContext = webpart inputContext
+            match outputContext with
+            | Some context ->
+                printfn "--------------"
+                printfn "Code : %d" context.Response.StatusCode
+                printfn "Output : %s" context.Response.Content
+                printfn "--------------"
+            | None ->
+                printfn "No Output"
+        } |> Async.RunSynchronously
+    
+    let parseRequest (input : System.String) =
+        let parts = input.Split([|';'|])
+        let rawType = parts.[0]
+        let route = parts.[1]
+        match rawType with
+            | "GET" -> {Type = GET; Route = route}
+            | "POST" -> {Type = POST; Route = route}
+            | _ -> failwith "invalid request"
+    
+    let executeInLoop inputContext webpart =
+        let mutable continueLooping = true
+        while continueLooping do
+            printf "Enter Input Route : "
+            let input = System.Console.ReadLine()
+            try
+                if input = "exit" then
+                    continueLooping <- false
+                else
+                    let context = {inputContext with Request = parseRequest input}
+                    execute context webpart
+            with
+                | ex ->
+                    printfn "Error : %s" ex.Message
+
+module Combinators =
+    // WebPart -> WebPart -> WebPart
+    let compose first second context = async {
+        let! firstContext = first context
+        match firstContext with
+            | None -> return None
+            | Some context ->
+                let! secondContext = second context
+                return secondContext
+    }
+    let (>=>) = compose
+
+module Filters =
+    open Http
+
+    // (Context -> bool) -> Context -> Async<Context option>
+    let iff condition context =
+        if condition context then
+            context |> Some |> async.Return
+        else
+            None |> async.Return
+
+    let GET = iff (fun context -> context.Request.Type = GET)
+    let POST = iff (fun context -> context.Request.Type = POST)
+    let Path path = iff (fun context -> context.Request.Route = path)
+
+    // WebPart list -> WebPart
+    let rec Choose webparts context = async {
+        match webparts with
+            | [] -> return None
+            | x :: xs ->
+                let! result = x context
+                match result with
+                    | Some x -> return Some x
+                    | None -> return! Choose xs context
+    }
